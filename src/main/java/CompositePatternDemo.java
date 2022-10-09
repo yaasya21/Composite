57cc5c6c36d5020f09b443d1c3f2fb7f3adaf809
Oleksandra Hryshchenko <galexa0320@gmail.com>
2022-10-09 12:51:58 +0200

    Commit 2: some cosmetic changes

--- src/main/java/CompositePatternDemo.java
+++ src/main/java/CompositePatternDemo.java
@@ -9,16 +9,29 @@ public class CompositePatternDemo {
 
         Employee manager1 = new Employee("Dmitriy Bondarev", "Management", 10000, managerBoss.getID());
         Employee manager2 = new Employee("Volodimir Shishkin", "Management", 10000, managerBoss.getID());
-        salesBoss.add(manager1);
-        salesBoss.add(manager2);
+        managerBoss.add(manager1);
+        managerBoss.add(manager2);
 
         Employee salesman1 = new Employee("Stanislav Borshch", "Sales", 5000, salesBoss.getID());
         Employee salesman2 = new Employee("Grigoriy Pivovarow", "Sales", 5000, salesBoss.getID());
         salesBoss.add(salesman1);
         salesBoss.add(salesman2);
 
-        mainBoss.remove(managerBoss);
+        System.out.println(mainBoss.toString());
+        for(Employee bossEmployee : mainBoss.getSubordinates()) {
+            System.out.println(bossEmployee.toString());
+            for(Employee simpleEmployee : bossEmployee.getSubordinates()) {
+                System.out.println(simpleEmployee.toString());
+            }
+        }
 
+        System.out.println();
+        mainBoss.remove(managerBoss);
+        System.out.println("His department was hired with him :(");
+        /* manager1 = new Employee("Dmitriy Bondarev", "Management", 10000, mainBoss.getID()); // if you want not to hire all department with their boss
+        manager2 = new Employee("Volodimir Shishkin", "Management", 10000, mainBoss.getID());
+        mainBoss.add(manager1);
+        mainBoss.add(manager2); */
         System.out.println(mainBoss.toString());
         for(Employee bossEmployee : mainBoss.getSubordinates()) {
             System.out.println(bossEmployee.toString());
